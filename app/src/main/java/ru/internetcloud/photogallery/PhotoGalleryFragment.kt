4b2e764dd974ba0f999de6f8e505d2e57afb8040
harisov-mobile <v8spec@mail.ru>
2021-07-12 11:17:01 +0300

    добавлен RecyclerView, он выводит только текстовые заголовки

--- app/src/main/java/ru/internetcloud/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/ru/internetcloud/photogallery/PhotoGalleryFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -55,8 +56,35 @@ class PhotoGalleryFragment: Fragment() {
         photoGalleryViewModel.galleryItemLiveData.observe(
             viewLifecycleOwner,
             Observer { galleryItems -> Log.i(TAG, "Have gallery items from ViewModel")
+                // Обновить данные, поддерживающие представление RecyclerView
+                photoRecyclerView.adapter = PhotoAdapter(galleryItems)
             }
         )
+    }
+
+    // внутренний класс PhotoHolder
+    private class PhotoHolder(itemTextView: TextView) : RecyclerView.ViewHolder(itemTextView) {
+
+        val bindTitle: (CharSequence) -> Unit = itemTextView::setText // лямбда-функция
+    }
+
+    // внутренний класс PhotoAdapter
+    private class PhotoAdapter(private val galleryItems: List<GalleryItem>) : RecyclerView.Adapter<PhotoHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
+
+            val textView = TextView(parent.context)
+
+            return PhotoHolder(textView)
+        }
+
+        override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
+            val galleryItem = galleryItems[position]
+            holder.bindTitle(galleryItem.title)
+        }
+
+        override fun getItemCount(): Int {
+            return galleryItems.size
+        }
     }
 }
 
